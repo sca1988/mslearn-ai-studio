70db21cb6c97c1b74c5f3ea81e8838a9cb4f309a
Simone Cammarano <simone.cammarano@infominds.eu>
2026-05-23 16:40:46 +0200

    async sdk

--- labfiles/foundry-chat/python/chat-app/chat-async.py
+++ labfiles/foundry-chat/python/chat-app/chat-async.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 
 # import namespaces for async
-
+import asyncio
+from openai import AsyncOpenAI
+from azure.identity.aio import DefaultAzureCredential, get_bearer_token_provider
 
 
 async def main(): 
@@ -17,7 +19,15 @@ async def main():
         model_deployment = os.getenv("MODEL_DEPLOYMENT")
 
         # Initialize an async OpenAI client
+        credential = DefaultAzureCredential()
+        token_provider = get_bearer_token_provider(
+        credential, "https://ai.azure.com/.default"
+        )
 
+        async_client = AsyncOpenAI(
+            base_url=azure_openai_endpoint,
+            api_key=token_provider
+        )
         
 
         # Track responses
@@ -33,7 +43,16 @@ async def main():
                 continue
 
             # Await an asynchronous response
-
+            response = await async_client.responses.create(
+                        model=model_deployment,
+                        instructions="You are a helpful AI assistant that answers questions and provides information.",
+                        input=input_text,
+                        previous_response_id=last_response_id,
+                        max_output_tokens=100
+            )
+            assistant_text = response.output_text
+            print("Assistant:", assistant_text)
+            last_response_id = response.id
             
 
     except Exception as ex:
@@ -41,7 +60,7 @@ async def main():
 
     finally:
         # Close the async client session
-
+        await credential.close()
 
 
 if __name__ == '__main__': 
